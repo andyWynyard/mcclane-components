56466ae0b61f87af6ed1665e7cd6a6beafbeae0b
andyWynyard <andrew.wynyard@gmail.com>
2020-07-14 15:33:13 +0200

    Library changes

--- dist/H1/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-var H1 = styled.h1.withConfig({
-  displayName: "H1",
-  componentId: "sc-1o8bxey-0"
-})(["background-color:tomato;color:rebeccapurple;"]);
-export default H1;
